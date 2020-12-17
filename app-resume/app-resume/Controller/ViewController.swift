6117991b3fcffb7181900227751ce303202571a1
mailys <mailys.perez@epitech.eu>
2020-12-17 12:00:11 +0100

    init project - Impl MVC pattern

---
+++ app-resume/app-resume/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-resume
+//
+//  Created by Maïlys Perez on 17/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
